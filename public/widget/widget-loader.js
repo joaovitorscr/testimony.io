c282c970b33c859708f7932cdaa3f1c495e45011
joaovitorscr <98563963+joaovitorscr@users.noreply.github.com>
2026-02-01 23:10:10 -0300

    FIX: Widget loader api url

--- public/widget/widget-loader.js
+++ public/widget/widget-loader.js
@@ -27,7 +27,7 @@
   const embedDomain = window.location.origin; // This will be the origin of the iframe (e.g., http://localhost:3000)
 
   const input = { json: { widgetId, domain: embedDomain } };
-  const apiUrl = `http://localhost:3000/api/trpc/widget.getWidgetContent?input=${encodeURIComponent(
+  const apiUrl = `http://${window.location.hostname}:${window.location.port}/api/trpc/widget.getWidgetContent?input=${encodeURIComponent(
     JSON.stringify(input),
   )}`;
 
